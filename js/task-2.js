61dcdd6c62e3d5175df72f8e30af371550af4627
Sofiia <atamaniuksofiia@gmail.com>
2024-07-12 13:14:01 +0300

    add js code

---
+++ js/task-2.js
@@ -0,0 +1,9 @@
+`use strict`
+function getShippingMessage(country, price, deliveryFee){
+    let totalPrice = price + deliveryFee;
+return `Shipping to ${country} will cost ${totalPrice} credits`
+}
+
+getShippingMessage("Australia", 120, 50)
+getShippingMessage("Germany", 80, 20)
+getShippingMessage("Sweden", 100, 20)
