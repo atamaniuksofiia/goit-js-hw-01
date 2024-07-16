fa62442339c374baede4ea678c97c84ba356ed90
Sofiia <atamaniuksofiia@gmail.com>
2024-07-16 12:30:48 +0300

    update js code

--- js/task-2.js
+++ js/task-2.js
@@ -7,3 +7,7 @@ return `Shipping to ${country} will cost ${totalPrice} credits`
 getShippingMessage("Australia", 120, 50)
 getShippingMessage("Germany", 80, 20)
 getShippingMessage("Sweden", 100, 20)
+
+console.log(getShippingMessage("Australia", 120, 50)); 
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20)); 
